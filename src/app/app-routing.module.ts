b4b79339ea451e1ca123b1d238e7f5b0d2dafed3
Janakaraj <janak@promactinfo.com>
2020-09-25 18:28:53 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
+import { DepartmentComponent } from './department/department.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'employee', component: EmployeeComponent},
+  { path: 'department', component: DepartmentComponent },
+  { path: 'profile/:id', component: EmployeeProfileComponent },
+  { path: 'departmentDetails/:id', component: DepartmentDetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { AdminComponent } from './admin/admin.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeComponent,
+    DepartmentComponent,
+    AdminComponent,
+    DeveloperComponent,
+    EmployeeProfileComponent,
+    DepartmentDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
